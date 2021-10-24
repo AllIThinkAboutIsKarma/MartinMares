6b835be3123c84dcc11cfdd1fb1dcf0c055d9c5b
AllIThinkAboutIsKarma <martinmarescr@gmail.com>
2021-10-24 14:05:26 -0500

    Landing page
    Página principal creada para pc y móvil, falta terminar el menú hamburguesa

---
+++ script.js
@@ -0,0 +1,70 @@
+
+var uno = document.getElementById("e1");
+var dos = document.getElementById("e2");
+var tres = document.getElementById("e3");
+var menuToogle = document.getElementById("nav-links");
+
+function inicio() {
+    uno.classList.add("Cblack");
+    dos.classList.add("Cblack");
+    tres.classList.add("Cblack");
+    menuToogle.classList.add("menuInvisible");
+};
+
+inicio();
+
+function menui() {
+    menuToogle.classList.remove("menuInvisible");
+    menuToogle.classList.add("menuVisible");
+};
+
+function hoverover1() {
+    
+    uno.classList.remove("Cblack");
+    uno.classList.add("Cwhite");
+};
+
+function hoverover2() {
+
+    dos.classList.remove("Cblack");
+    dos.classList.add("Cwhite");
+}
+
+function hoverover3() {
+
+    tres.classList.remove("Cblack");
+    tres.classList.add("Cwhite");
+}
+
+function hoverout1() {
+    
+    uno.classList.remove("Cwhite");
+    uno.classList.add("Cblack");
+};
+
+function hoverout2() {
+
+    dos.classList.remove("Cwhite");
+    dos.classList.add("Cblack");
+};
+
+function hoverout3() {
+
+    tres.classList.remove("Cwhite");
+    tres.classList.add("Cblack");
+};
+
+function Href0(){
+    window.open('https://wa.me/4495407591','_blank');
+}
+
+function Href1(){
+    window.open('https://www.linkedin.com/in/knobody-k-402221220/','_blank');
+}
+
+function Href2(){
+    window.open('https://github.com/AllIThinkAboutIsKarma','_blank');
+}
+
+
+
