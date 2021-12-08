76a84982ad4a6a8bc73ed2ce0e0dec6413465be9
AllIThinkAboutIsKarma <martinmarescr@gmail.com>
2021-12-08 17:33:03 -0600

    Summary

---
+++ projects.js
@@ -0,0 +1,6 @@
+var carrusel = [{
+   img1 :'/assets/img/ProyectoJaponViews.png', 
+}];
+
+var imagen1 = document.getElementById("projects"); 
+imagen1.innerHTML = '<img src="/assets/img/ProyectoJaponViews.png">';
